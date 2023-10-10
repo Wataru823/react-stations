80215227796a372463bcfd158d0cb099063adc1a
Wataru823 <voyage0823@gmail.com>
2023-10-11 00:37:07 +0900

    station3

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ export const App = () => {
   return (
     <div>
       <h2>Dogアプリ</h2>
+      <p>犬の画像を表示するサイトです。</p>
+      {/* Dog APIを使用 */}
+      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" alt="犬の画像" />
     </div>
   )
 }
