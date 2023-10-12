104eaaed352093e18068de43deed520e24d9bfd5
Wataru823 <voyage0823@gmail.com>
2023-10-12 12:33:56 +0900

    station6

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,20 @@ import './App.css'
 export const App = () => {
   const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg');
 
+  // fetchで'https://dog.ceo/api/breeds/image/random'を取得し、URLを更新
+  const getDogUrl = async () => {
+    const res = await fetch('https://dog.ceo/api/breeds/image/random');
+    const data = await res.json();
+    setDogUrl(data.message);
+  }
+
   return (
     <div>
       <h2>Dogアプリ</h2>
       <p>犬の画像を表示するサイトです。</p>
       <img src={dogUrl} alt="犬の画像" />
       <br />
-      <button onClick={() => setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')}>新しい犬の画像を表示</button>
+      <button onClick={getDogUrl}>更新</button>
     </div>
   )
 }
