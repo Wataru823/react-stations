e68b29d3f4ea75be24386a1e60e3513b50521765
Wataru823 <voyage0823@gmail.com>
2023-10-12 12:18:22 +0900

    station4

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-// DO NOT DELETE
-
+import React, { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg');
+
   return (
     <div>
       <h2>Dogアプリ</h2>
       <p>犬の画像を表示するサイトです。</p>
-      {/* Dog APIを使用 */}
-      <img src="https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg" alt="犬の画像" />
+      <img src={dogUrl} alt="犬の画像" />
     </div>
   )
 }
